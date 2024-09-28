29ab488e09ebe0511304be075871db4a59ec272b
h186607 <186607@stud.hvl.no>
2024-09-28 14:31:01 +0200

    fiks deklarasjon

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -96,7 +96,7 @@ public class Tabeller {
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-		int sammenSatt[] = new int[tabell1.length + tabell2.length];
+		int[] sammenSatt = new int[tabell1.length + tabell2.length];
 		
 		for (int i = 0; i < tabell1.length; i++) {
 			sammenSatt[i] = tabell1[i];
